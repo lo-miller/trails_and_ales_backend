c2c3a6ff51d153d463bb0be7fe2e3eebbf2190b5
Lauren Miller <lomiller0913@gmail>
2021-04-26 22:42:32 -0400

    remove commented out code

--- app/controllers/api/breweries_controller.rb
+++ app/controllers/api/breweries_controller.rb
@@ -1,10 +1,3 @@
 class Api::BreweriesController < ApplicationController
-  # def index
-  #   response = HTTP.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.589,-122.227&type=brewery&radius=5000&key=AIzaSyCF-BfvWPyMZJxg38qAF6v_cymjBhIsPKU')
-  #   render json: response.parse
-  # end
-
-  # def show
-  #   render "show.json.jb"
-  # end
+  
 end
