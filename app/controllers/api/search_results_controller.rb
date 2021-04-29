2e51bba7e63370edbb27b5b64ac7160f2379c413
Lauren Miller <lomiller0913@gmail>
2021-04-28 20:23:02 -0400

    update credentials

--- app/controllers/api/search_results_controller.rb
+++ app/controllers/api/search_results_controller.rb
@@ -1,7 +1,8 @@
 class Api::SearchResultsController < ApplicationController
   def index
     p params[:location]
-    # :params = {"location": params[:location]}
+    p params
+    # :params = {location: params[:location]}
     response = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?type=brewery&keyword=brewery&radius=5000&key=#{Rails.application.credentials.google_maps_api[:api_key]}", :params => {:location => params[:location]})
     render json: response.parse
   end
