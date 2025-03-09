743619e4f6dc1022338fc8291b7274d3adb6ba45
LAUREN MILLER <lomiller0913@gmail.com>
2025-03-09 16:12:13 -0400

    send features name list to front end

--- app/controllers/api/territories_controller.rb
+++ app/controllers/api/territories_controller.rb
@@ -1,7 +1,5 @@
 class Api::TerritoriesController < ApplicationController
   def index
-    p params[:location]
-    p params
     response = HTTP.get("https://native-land.ca/api/index.php?maps=territories", :params => {:position => params[:location]})
     render json: response.parse
   end
